050c214bb2ba0eb13d801a5d261961103cf98531
jke94 <jcarracedo.dev@gmail.com>
2023-09-30 23:39:46 +0200

    Redefinition of function order in header API file.

--- Logger/logger_api.h
+++ Logger/logger_api.h
@@ -6,17 +6,6 @@
 #include <cstring>
 #include <string>
 
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-
-typedef void (*INIT_LOGGER)(std::string msg);
-typedef void (*END_LOGGER)();
-
-void log_trace(const char* file, const char* function, const int line, const std::string& msg);
-void log_info(const char* file, const char* function, const int line, const std::string& msg);
-void log_warning(const char* file, const char* function, const int line, const std::string& msg);
-void log_error(const char* file, const char* function, const int line, const std::string& msg);
-void log_critical(const char* file, const char* function, const int line, const std::string& msg);
-
 #ifdef  __cplusplus
 
 extern "C" 
@@ -48,14 +37,28 @@ extern "C"
 //     #define WRITE_INFO(msg)
 // #endif
 
-void init_logger(std::string log_file_name);
-void end_logger();
+    typedef void (*INIT_LOGGER)(std::string msg);
+    void init_logger(std::string log_file_name);
 
-#define WRITE_TRACE(msg) log_trace( __FILENAME__, __FUNCTION__, __LINE__ , msg)
-#define WRITE_INFO(msg) log_info(__FILENAME__, __FUNCTION__, __LINE__ , msg)
-#define WRITE_WARNING(msg) log_warning(__FILENAME__, __FUNCTION__, __LINE__ , msg)
-#define WRITE_ERROR(msg) log_error(__FILENAME__, __FUNCTION__, __LINE__ , msg)
-#define WRITE_CRITICAL(msg) log_critical(__FILENAME__, __FUNCTION__, __LINE__ , msg)
+    typedef void (*END_LOGGER)();
+    void end_logger();
+
+    #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+    void log_trace(const char* file, const char* function, const int line, const std::string& msg);
+    #define WRITE_TRACE(msg) log_trace( __FILENAME__, __FUNCTION__, __LINE__ , msg)
+
+    void log_info(const char* file, const char* function, const int line, const std::string& msg);
+    #define WRITE_INFO(msg) log_info(__FILENAME__, __FUNCTION__, __LINE__ , msg)
+
+    void log_warning(const char* file, const char* function, const int line, const std::string& msg);
+    #define WRITE_WARNING(msg) log_warning(__FILENAME__, __FUNCTION__, __LINE__ , msg)
+
+    void log_error(const char* file, const char* function, const int line, const std::string& msg);
+    #define WRITE_ERROR(msg) log_error(__FILENAME__, __FUNCTION__, __LINE__ , msg)
+
+    void log_critical(const char* file, const char* function, const int line, const std::string& msg);
+    #define WRITE_CRITICAL(msg) log_critical(__FILENAME__, __FUNCTION__, __LINE__ , msg)
 
 #ifdef  __cplusplus
 }
